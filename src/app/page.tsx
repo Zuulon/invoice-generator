859ea38b71d973592269ba6431f9ef59943430d0
Zuulon <119450439+Zuulon@users.noreply.github.com>
2025-02-07 10:21:14 -0800

    Create page.tsx

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+import InvoiceGenerator from '@/components/invoice/InvoiceGenerator';
+
+export default function Home() {
+  return (
+    <main className="min-h-screen p-4">
+      <InvoiceGenerator />
+    </main>
+  );
+}
