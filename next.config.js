c63ed32878826e294e019b07eb3ff44828751844
Zuulon <119450439+Zuulon@users.noreply.github.com>
2025-02-07 10:27:53 -0800

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -2,6 +2,10 @@
 const nextConfig = {
   webpack: (config) => {
     config.resolve.alias.canvas = false;
+    config.resolve.fallback = {
+      ...config.resolve.fallback,
+      fs: false,
+    };
     return config;
   },
 }
